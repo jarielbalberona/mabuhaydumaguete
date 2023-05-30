238d4372685735b49b1b018ab20f4291ac5b5448
advenjar <hi@advenjar.com>
2023-05-30 18:47:24 +0800

    add homepage

---
+++ src/app/layout/navigation.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+
+import { classNames } from "~/utils/general";
+
+const Navigation = () => {
+  return (
+    <Disclosure as="nav" className="relative z-10 bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className="px-2 mx-auto sm:px-6 lg:px-8">
+            <div className="relative flex justify-between h-24">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                <div className="flex items-center flex-shrink-0">
+                  <Image
+                    className="block w-auto h-16 lg:hidden"
+                    src="/assets/img/mabugay.png"
+                    width={80}
+                    height={80}
+                    alt="Your Company"
+                  />
+                  <Image
+                    className="hidden w-auto h-16 lg:block"
+                    src="/assets/img/mabugay.png"
+                    alt="Your Company"
+                    width={80}
+                    height={80}
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 border-indigo-500"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Dives
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Accommodation
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Restaurant
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Gallery
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                  >
+                    About Us
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                  >
+                    FAQ
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="pt-2 pb-4 space-y-1">
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-indigo-700 border-l-4 border-indigo-500 bg-indigo-50"
+              >
+                Dashboard
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+              >
+                Team
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+              >
+                Projects
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+              >
+                Calendar
+              </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+};
+
+export default Navigation;
---
+++ src/app/page.tsx
@@ -0,0 +1,105 @@
+export default function Home() {
+  return (
+    <main>
+      <div
+        className="h-[75vh]  text-white"
+        style={{
+          backgroundImage: "url('/assets/img/home-bg-2.jpg')",
+        }}
+      >
+        <div className="bubbles">
+          <div className="bubble x1"></div>
+          <div className="bubble x2"></div>
+          <div className="bubble x3"></div>
+          <div className="bubble x4"></div>
+          <div className="bubble x5"></div>
+          <div className="bubble x6"></div>
+          <div className="bubble x7"></div>
+          <div className="bubble x8"></div>
+          <div className="bubble x9"></div>
+          <div className="bubble x10"></div>
+        </div>
+        <div className="flex items-center justify-center h-full">
+          <h1 className="flex flex-col text-center drop-shadow-md">
+            <span className="text-2xl">Welcome to</span>
+            <span className="font-bold text-8xl">Mabuhay Dumaguete</span>
+            <span className="mt-2 text-6xl">Dive Center</span>
+          </h1>
+        </div>
+      </div>
+      <div className="relative h-[50vh] bg-white">
+        <div className="flex items-center h-full px-8 py-12 mx-auto">
+          <p className="flex flex-col w-full text-2xl text-center">
+            <span>
+              Your ultimate destination for diving adventure and culinary
+              delights!
+            </span>
+            <span>
+              Our shop offers a unique blend of thrilling underwater experiences
+              and mouthwatering cuisine.
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="flex w-full">
+        <div
+          className="relative h-[75vh] p-12 overflow-hidden text-center bg-no-repeat bg-cover w-full"
+          style={{
+            backgroundImage: "url('/assets/img/scuba.jpeg')",
+          }}
+        >
+          <div
+            className="absolute top-0 bottom-0 left-0 right-0 w-full h-full overflow-hidden bg-fixed"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.6)",
+            }}
+          >
+            <div className="flex items-center justify-center h-full">
+              <div className="text-white">
+                <h2 className="mb-4 text-4xl font-semibold">Scuba</h2>
+                <h4 className="mb-6 text-xl font-semibold">Subheading</h4>
+                <button
+                  type="button"
+                  className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Call to action
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="relative h-[75vh] p-12 overflow-hidden text-center bg-no-repeat bg-cover w-full"
+          style={{
+            backgroundImage: "url('/assets/img/freedive.png')",
+          }}
+        >
+          <div
+            className="absolute top-0 bottom-0 left-0 right-0 w-full h-full overflow-hidden bg-fixed"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.6)",
+            }}
+          >
+            <div className="flex items-center justify-center h-full">
+              <div className="text-white">
+                <h2 className="mb-4 text-4xl font-semibold">Freediving</h2>
+                <h4 className="mb-6 text-xl font-semibold">Subheading</h4>
+                <button
+                  type="button"
+                  className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Call to action
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
